b9a04f7232c9175aa712bb558a0745539c5fd69f
MaksSzkorupa <maksszkorupa@Interia.pl>
2024-01-10 19:59:26 +0100

    Alpha

--- main.php
+++ main.php
@@ -3,9 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
     <title>Document</title>
 </head>
 <body>
-    
+    <div id="hero">
+        <div class="hero">
+            <div class="banner">
+                <div class="image">
+                    <h1>Jet Broker Clients</h1>
+                </div>
+                <nav>
+                    <ul>
+                        <a href="main.php"><li>Strona główna</li></a>
+                        <a href="list.php"><li>Lista klientów</li></a>
+                        <a href="login.php"><li>Wyloguj się</li></a>
+                    </ul>
+                </nav>
+            </div>
+            <div class="background">
+            <div class="background-image">
+                <h2>Sprawdź liste obecnych klientów</h2>
+                <div class="button-box">
+                    <a href="list.php"><button>Lista klientów</button></a>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
